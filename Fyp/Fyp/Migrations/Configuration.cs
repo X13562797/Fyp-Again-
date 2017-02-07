fbe5487322b33a8cb835c287a9d1f40837b054a8
Kevin Moss <x13562797@student.ncirl.ie>
2017-02-07 09:15:26 +0000

    Two Factor e-mail/sms , password recovery , login + register

---
+++ Fyp/Fyp/Migrations/Configuration.cs
@@ -0,0 +1,89 @@
+namespace Fyp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Fyp.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Fyp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        bool AddUserAndRole(Fyp.Models.ApplicationDbContext context)
+        {
+            IdentityResult ir;
+            var rm = new RoleManager<IdentityRole>
+                (new RoleStore<IdentityRole>(context));
+            ir = rm.Create(new IdentityRole("canEdit"));
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+            var user = new ApplicationUser()
+            {
+                UserName = "user1@contoso.com",
+            };
+            ir = um.Create(user, "P_assw0rd1");
+            if (ir.Succeeded == false)
+                return ir.Succeeded;
+            ir = um.AddToRole(user.Id, "canEdit");
+            return ir.Succeeded;
+        }
+
+        protected override void Seed(Fyp.Models.ApplicationDbContext context)
+        {
+            AddUserAndRole(context);
+            context.Patients.AddOrUpdate(p => p.Name,
+               new Patient
+               {
+                   Name = "Debra Garcia",
+                   Address = "1234 Main St",
+                   City = "Redmond",
+                   State = "WA",
+                   Zip = "10999",
+                   Email = "debra@example.com",
+               },
+                new Patient
+                {
+                    Name = "Thorsten Weinrich",
+                    Address = "5678 1st Ave W",
+                    City = "Redmond",
+                    State = "WA",
+                    Zip = "10999",
+                    Email = "thorsten@example.com",
+                },
+                new Patient
+                {
+                    Name = "Yuhong Li",
+                    Address = "9012 State st",
+                    City = "Redmond",
+                    State = "WA",
+                    Zip = "10999",
+                    Email = "yuhong@example.com",
+                },
+                new Patient
+                {
+                    Name = "Jon Orton",
+                    Address = "3456 Maple St",
+                    City = "Redmond",
+                    State = "WA",
+                    Zip = "10999",
+                    Email = "jon@example.com",
+                },
+                new Patient
+                {
+                    Name = "Diliana Alexieva-Bosseva",
+                    Address = "7890 2nd Ave E",
+                    City = "Redmond",
+                    State = "WA",
+                    Zip = "10999",
+                    Email = "diliana@example.com",
+                }
+                );
+        }
+    }
+}
---
+++ Fyp/Fyp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fyp.Startup))]
+namespace Fyp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
